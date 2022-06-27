125dbd7f0efff9a24e89ce538e466c576f5eb2f9
Sunil Paudel <Sunil.Paudel@kp.org>
2022-06-27 08:27:37 -0700

    Initila Load

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,26 @@
+import{Component} from 'react';
+
+class CardList extends Component {
+    render() {
+        //console.log('render from card-list.component.jsx');
+        const {monsters} = this.props;
+       return(
+        <div className='card-list'>
+            {monsters.map(monster => {
+                const { name, email, id } = monster;
+                return(
+                <div className='card-container' key={id}>
+                    <img alt={`monster ${name}`}
+                    src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                    />
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+
+            )})}
+        </div>
+       );
+    }
+}
+
+export default CardList;
