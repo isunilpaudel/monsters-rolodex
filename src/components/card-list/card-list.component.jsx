7f1f765556e45f4c88c7a64f24e6113a914cb799
Sunil Paudel <Sunil.Paudel@kp.org>
2022-07-13 15:07:54 -0700

    Changed Classes to Functinal Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,12 @@
-import { Component } from "react";
 import "./card-list.styles.css";
 import Card from "../card/card.component.jsx";
 
-class CardList extends Component {
-  render() {
-    //console.log('render from card-list.component.jsx');
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>;
+};
 
 export default CardList;
